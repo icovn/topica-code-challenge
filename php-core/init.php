60c22b6515b95bfad15d4075272f31c502f91967
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2018-05-24 06:11:08 +0700

    update setting

--- php-core/init.php
+++ php-core/init.php
@@ -5,11 +5,13 @@
 
     ini_set('date.timezone', 'Asia/Ho_Chi_Minh');
 
-    ini_set('session.save_handler', 'redis');
-    ini_set('session.save_path', 'tcp://127.0.0.1:6379');
     ini_set('session.hash_function', 1);
     ini_set('session.hash_bits_per_character', 4);
+    ini_set('session.cookie_domain', '.icovn.me' );
     ini_set('session.name', 'my-secure-session');
+
+    ini_set('session.save_handler', 'redis');
+    ini_set('session.save_path', 'tcp://127.0.0.1:6379');
 ?>
 
 <?php
--- php-framework/silex/app/web/index.php
+++ php-framework/silex/app/web/index.php
@@ -3,10 +3,12 @@
 //init PHP settings
 ini_set('default_mimetype', 'text/html');
 ini_set('default_charset', 'UTF-8');
+
 ini_set('date.timezone', 'Asia/Ho_Chi_Minh');
-ini_set('session.cookie_domain', '.icovn.me' );
+
 ini_set('session.hash_function', 1);
 ini_set('session.hash_bits_per_character', 4);
+ini_set('session.cookie_domain', '.icovn.me' );
 ini_set('session.name', 'my-secure-session');
 
 
