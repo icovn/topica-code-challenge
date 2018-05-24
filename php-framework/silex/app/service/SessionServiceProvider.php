e3392f3abca6e5fa56a5a679d394af0c62da10c3
Nguyễn Quang Huy <quanghuy.ico@gmail.com>
2018-05-24 07:31:33 +0700

    try again

--- php-framework/silex/app/service/SessionServiceProvider.php
+++ php-framework/silex/app/service/SessionServiceProvider.php
@@ -14,9 +14,45 @@ class SessionServiceProvider extends \Silex\Provider\SessionServiceProvider
 {
     public function register(Container $app)
     {
-        parent::register($app);
+        $app['session.test'] = false;
+
+        $app['session'] = function ($app) {
+            return new Session($app['session.storage'], $app['session.attribute_bag'], $app['session.flash_bag']);
+        };
+
+        $app['session.storage'] = function ($app) {
+            if ($app['session.test']) {
+                return $app['session.storage.test'];
+            }
+
+            return $app['session.storage.native'];
+        };
 
         $this::initSessionStorage();
+
+        $app['session.storage.native'] = function ($app) {
+            return new NativeSessionStorage(
+                $app['session.storage.options'],
+                $app['session.storage.handler']
+            );
+        };
+
+        $app['session.listener'] = function ($app) {
+            return new SessionListener($app);
+        };
+
+        $app['session.storage.test'] = function () {
+            return new MockFileSessionStorage();
+        };
+
+        $app['session.listener.test'] = function ($app) {
+            return new TestSessionListener($app);
+        };
+
+        $app['session.storage.options'] = [];
+        $app['session.storage.save_path'] = null;
+        $app['session.attribute_bag'] = null;
+        $app['session.flash_bag'] = null;
 
         $app['monolog']->info("init redis session done");
     }
