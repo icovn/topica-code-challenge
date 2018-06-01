1c4a985f574e010ae6402e94c9c84f5baeccd7a4
huynq12 <huynq12@topica.edu.vn>
2018-06-01 17:33:44 +0700

    nodejs ok

--- javascript-feathersjs/src/hooks/enrich-oauth-2-user.js
+++ javascript-feathersjs/src/hooks/enrich-oauth-2-user.js
@@ -1,11 +1,17 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 
+const feathers = require('@feathersjs/feathers');
+const jsonStringify = require('json-stringify-safe');
+const logger = require('winston');
+
 const rest = require('../_helper/oauth-icovn-rest-client');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
+    logger.info('context %s', jsonStringify(context));
+    const { app, data } = context;
 
     if (context.params.oauth) {
       // do something for all OAuth providers
@@ -13,15 +19,23 @@ module.exports = function (options = {}) {
 
     if (context.params.oauth && context.params.oauth.provider === 'icovn') {
       // do something specific to the icovn provider
+      const profile = await rest.getProfile(data.icovn.accessToken);
+      // logger.info('profile %s', jsonStringify(profile));
+      data.email = profile.data.name;
+      data.source = 'oauth2-icovn';
 
-      const message = context.data;
+      const dbUser = await app.service('users').find({
+        query: {
+          email: data.email
+        }
+      });
+      logger.info('dbUser %s', jsonStringify(dbUser));
 
-      const email = await rest.getProfile(message.icovn.accessToken);
-
-      message.email = email.data.name;
-      message.source = 'oauth2-icovn';
-
+      if(dbUser.total > 0){
+        context.result = "SKIP INSERT NEW USER";
+      }
     }
+
     return context;
   };
 };
